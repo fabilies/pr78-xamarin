19597b690fcd6f82d8b502714b55911c4ff73b14
fabilies <fabian080@live.nl>
2018-06-04 12:01:42 +0200

    Commit #1
    First commit

---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public void ClickTap1(object sender, EventArgs e)
+        {
+            var page = new Page1();
+            MainView.Content = page.Content;
+        }
+
+        public void ClickTap2(object sender, EventArgs e)
+        {
+            var page = new Page2();
+            MainView.Content = page.Content;
+        }
+
+        public void ClickTap3(object sender, EventArgs e)
+        {
+            var page = new Page3();
+            MainView.Content = page.Content;
+        }
+
+        public void ClickTap4(object sender, EventArgs e)
+        {
+            var page = new Page4();
+            MainView.Content = page.Content;
+            MainView.ControlTemplate = page.ControlTemplate;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MobileApp/MobileApp/Page3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : ContentPage
+	{
+		public Page3 ()
+		{
+			InitializeComponent ();
+		}
+	}
+
+    //private void switch2_Toggled(object sender, ToggledEventArgs e)
+    //{
+    //    bool value = e.Value;
+
+    //}
+
+}
